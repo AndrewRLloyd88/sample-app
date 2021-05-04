42df5b45c78017531f78b763b4ce69fa7a40806d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-05-04 16:52:45 -0700

    fix test for root route to not include home in title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+    base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  # Checks to see if an input string is a palindrome
+  def palindrome_tester(s)
+    result = false
+    if s == s.reverse
+      result = true
+      return result
+    else
+      result = false
+      return result
+    end
+  end
+ 
+  # Returns an array as an uppercase string
+ def yeller(s)
+   puts s.map(&:upcase).join
+ end
+
+ # Randomly generates an 8 character string
+ def random_subdomain
+   puts ("a".."z").to_a.sample(8).join
+ end
+
+ # Randomly shuffles letters around in an array and returns string
+ def string_shuffle(s)
+  puts s.split('').shuffle.join
+ end
+
 end
